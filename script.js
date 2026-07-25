36bfe9c7df51e8b853d43375ec06edb645a5e8f8
Luis Renato da Silva <lukartes28@gmail.com>
2026-07-24 22:31:28 -0300

    ajustes

--- script.js
+++ script.js
@@ -7,3 +7,28 @@
         setTimeout(()=> btn.innerText = textoOriginal, 1800);
       });
     }
+
+
+    const btnMobile = document.getElementById('btn-mobile');
+const navLinks = document.getElementById('nav-links');
+
+btnMobile.addEventListener('click', function () {
+  navLinks.classList.toggle('active');
+
+  const icon = btnMobile.querySelector('i');
+  icon.classList.toggle('fa-bars');
+  icon.classList.toggle('fa-xmark');
+
+  const isOpen = navLinks.classList.contains('active');
+  btnMobile.setAttribute('aria-expanded', isOpen);
+});
+
+// fecha o menu ao clicar em qualquer link
+navLinks.querySelectorAll('a').forEach(function (link) {
+  link.addEventListener('click', function () {
+    navLinks.classList.remove('active');
+    btnMobile.querySelector('i').classList.add('fa-bars');
+    btnMobile.querySelector('i').classList.remove('fa-xmark');
+    btnMobile.setAttribute('aria-expanded', false);
+  });
+});
